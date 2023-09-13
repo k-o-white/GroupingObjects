fd72003ecb7a38d2c13cc457d5944580d701266d
k-o-white <koburilichev@gmail.com>
2023-09-13 09:21:42 +0400

    Header file ReadWrite.h added

---
+++ include/ReadWrite.h
@@ -0,0 +1,17 @@
+//
+// Created by teena on 13.09.2023.
+//
+
+#pragma once
+#include <iostream>
+#include <vector>
+#include <map>
+#include <locale>
+#include <fstream>
+#include <sstream>
+#include <codecvt>
+#include "Object.h"
+
+std::vector<Object*> readFromFile(std::string filePath);
+
+void writeToFile(std::map<std::wstring, std::vector<Object*>> &groups);
